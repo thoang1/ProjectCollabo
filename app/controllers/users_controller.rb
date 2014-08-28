6d156c2b14c2daaa1e62a700b50b36c6a22e6a08
Tyler Oliver <tyler8oliver@gmail.com>
2014-08-27 22:19:21 -0400

    Added profile v1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
-	
+	def show
+    @user = User.find_by_profile_name(params[:id])
+  end
+
+  def index 
+  	@users = User.all
+  end
+
+  def edit
+    @user = current_user
+  end
+
+ def update
+    @user = User.find_by_profile_name(params[:id])
+    @user.update(user_params)
+    redirect_to @user
+	end
+
+  def user_params
+      params.require(:user).permit(:about_me, :hobbies, :country, :gender)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+  resources :users
   resources :dashboard
   devise_scope :user do
     get 'register', to: "devise/registrations#new", as: :register
