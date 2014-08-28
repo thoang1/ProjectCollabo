537027b7f0c3fe501f60c1b75b6c2979f0537c0c
Tyler Oliver <tyler8oliver@gmail.com>
2014-08-27 22:59:25 -0400

    Added views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+	before_filter :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
 	def show
-    @user = User.find_by_profile_name(params[:id])
+    @user = User.find_by_profile_name(params[:id]) 
+    if (@user)
+			render action: :show
+    else
+      render file: 'public/404', status: 404, formats: [:html]
+    end
   end
 
   def index 
