4fa894995534cc0c2bfbbc3dfa449ddd529bc505
Tyler Oliver <tyler8oliver@gmail.com>
2014-09-03 09:34:41 -0400

    Added index pages on static pages

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
     end
   end
   resources :relationships, only: [:create, :destroy]
+  get "events/index"
+  get "forums/index"
+  get "projects/index"
+  get "stories/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
